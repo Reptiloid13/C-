fcf53f781210668d41e7094bdc2c3fee7897c4c1
Sarkis <petrosyants.s@yandex.ru>
2022-09-30 17:11:19 +0300

    Love Kri

---
+++ Example001/Examole002/Кристина/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "Кристина")
+{
+      Console.WriteLine ("Ура!! это же любимая Кристюша!");
+}
+else
+{
+Console.WriteLine ("Привет, ");
+Console.WriteLine (username) ;
+}
